de30c6b6ee21adc2ffb3c0ac26cec5a4f27e6adf
kousukef <kousuke@yahoo.co.jp>
2020-05-05 12:13:57 +0000

    finish product-new-page exept for design

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,11 @@ class ProductsController < ApplicationController
   end
   
   def create
-    @product = current_user.products.build(product_params)
+    p = product_params
+    product_p = {name: p[:name], description: p[:description], price: p[:price], product_image: p[:product_image]}
+    @product = current_user.products.build(product_p)
+    category = ProductCategory.find_by(name: p[:product_category][:ancestry])
+    @product.product_category_id = category.id
     if @product.save
       redirect_to products_url, notice: '作成に成功'
     else
@@ -38,6 +42,6 @@ class ProductsController < ApplicationController
   private
   
     def product_params
-      params.require(:product).permit(:name, :description, :price, :product_image)
+      params.require(:product).permit(:name, :description, :price, :product_image,product_category: [:ancestry])
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,41 +2,41 @@
 books =     [{children: '教科書', grandchildren: ['教科書（経営学部）', '教科書（経済学部）', '教科書（コミュニケーション学部）',
                                                   '教科書（現代法学部）','教科書（その他）']},
              {children: '参考書'},
-             {children: '漫画',   grandchildren: ['少年漫画', '青年漫画', '少女漫画']},
+             {children: '漫画',   grandchildren: ['少年漫画', '青年漫画', '少女漫画', 'その他(漫画)']},
              {children: '雑誌'},
              {children: '小説'},
              {children: 'ビジネス書'},
-             {children: 'その他'}]
+             {children: 'その他(本)'}]
 
-mens =      [{children: '服', grandchildren: ['トップス', 'ジャケット/アウター', 'パンツ', 'その他']},
-             {children: '靴', grandchildren: ['革靴', 'スニーカー', 'サンダル', 'その他']},
+mens =      [{children: '服', grandchildren: ['トップス', 'ジャケット/アウター', 'パンツ', 'その他(服/メンズ)']},
+             {children: '靴', grandchildren: ['革靴', 'スニーカー', 'サンダル', 'その他(靴/メンズ)']},
              {children: 'カバン', grandchildren: ['リュック', 'トートバッグ', 'ショルダーバッグ', 'ボストンバッグ', 'ボディーバッグ',
-                                                 'ビジネスバッグ', 'その他']},
+                                                 'ビジネスバッグ', 'その他(カバン/メンズ)']},
              {children: 'アクセサリー'},
              {children: '小物'},
-             {children: 'その他'}]
+             {children: 'その他(メンズ)'}]
         
-ledys =     [{children: '服', grandchildren: ['トップス', 'ジャケット/アウター', 'パンツ', 'その他']},
-             {children: '靴', grandchildren: ['革靴', 'スニーカー', 'サンダル', 'その他']},
+ledys =     [{children: '服', grandchildren: ['トップス', 'ジャケット/アウター', 'パンツ', 'その他(服/レディース)']},
+             {children: '靴', grandchildren: ['革靴', 'スニーカー', 'サンダル', 'その他(靴/レディース)']},
              {children: 'カバン', grandchildren: ['リュック', 'トートバッグ', 'ショルダーバッグ', 'ボストンバッグ', 'ボディーバッグ',
-                                                 'ビジネスバッグ', 'その他']},
+                                                 'ビジネスバッグ', '(カバン/レディース)']},
              {children: 'アクセサリー'},
              {children: '小物'},
-             {children: 'その他'}]
+             {children: 'その他(レディース)'}]
 
 gadgets =   [{children: 'スマホアクセサリー'},
              {children: 'PC'},
              {children: 'カメラ'},
-             {children: 'オーディオ機器', grandchildren: ['イヤホン', 'ヘッドホン', 'スピーカー', 'ポータブルプレーヤー', 'その他']},
-             {children: 'その他'}]
+             {children: 'オーディオ機器', grandchildren: ['イヤホン', 'ヘッドホン', 'スピーカー', 'ポータブルプレーヤー', 'その他(オーディオ機器)']},
+             {children: 'その他(ガジェット)'}]
 
 
-        
+#親カテゴリー
 categorys = [{parent: '本',         childs: books},
              {parent: 'メンズ',     childs: mens},
              {parent: 'レディース', childs: ledys},
              {parent: 'ガジェット', childs: gadgets},
-             {parent: 'その他',}]
+             {parent: 'その他'}]
              
              
 #カテゴリー作成
