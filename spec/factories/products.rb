5e331a00744966e04c579fb0275665e2b0b3ce77
kousukef <kousukef123@yahoo.co.jp>
2020-06-02 23:09:10 +0900

    wrote specs

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name {'product_1'}
     description {'this is a product'}
     price {3000}
-    images {Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpeg')) }
+    images { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpeg')) }
     association :seller
     association :category
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user , aliases: [:seller] do
+  factory :user, aliases: [:seller] do
     name {'user_1'}
     sequence(:email) {|n| "user_#{n}@example.com"}
     password {'password'}
